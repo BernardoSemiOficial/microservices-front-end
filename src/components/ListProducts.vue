f19edb281f778be5ac57e0cb167f9e90fef90abb
Matheus Barbieri <abarbierijunior@gmail.com>
2022-09-21 21:54:17 -0300

    Created list

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -1,8 +1,29 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   name: "ListProducts",
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      axios
+        .get("http://localhost:3000/products")
+        .then((res) => {
+          this.products = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 });
 </script>
 
@@ -10,6 +31,57 @@ export default defineComponent({
   <section class="form">
     <h1>Lista de produtos</h1>
   </section>
+
+  <table class="styled-table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Points</th>
+      </tr>
+    </thead>
+    <tbody v-for="product in products">
+      <tr>
+        <td>{{ product.title }}</td>
+        <td>{{ product.price }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<style scoped></style>
+<style scoped>
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+</style>
